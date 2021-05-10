d33178fdd52592422380d83522e96e207ebc4cdd
frankyfish <fr.fish93@gmail.com>
2021-05-10 22:47:30 +0300

    added working javax validation

--- src/main/java/com/example/springonspring/rest/GreetingResource.java
+++ src/main/java/com/example/springonspring/rest/GreetingResource.java
@@ -1,6 +1,7 @@
 package com.example.springonspring.rest;
 
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Collections;
 import java.util.Map;
 
+@Validated
 @RestController // combines: @Controller, @ResponseBody
 @RequestMapping(path = GreetingResource.BASE_PATH)
 public class GreetingResource {
@@ -23,11 +26,10 @@ public class GreetingResource {
     public String greet() {
         return "Greetings fellow Stranger!"; // returns plain string, not a JSON
     }
-
+    //https://www.baeldung.com/javax-validation
     @GetMapping
     @RequestMapping(path = HELLO_PATH + "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> greet(@PathVariable @NotBlank String name) { // todo: check why's not working
-        // receive a JSON as a response; otherwise it is a simple string
+    public Map<String, String> greet(@Size(min = 1, max = 5) @PathVariable String name) {
         return Collections.singletonMap("response", String.format("Hi %s !", name));
     }
 
